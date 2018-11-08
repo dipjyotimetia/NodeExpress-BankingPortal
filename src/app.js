841173e30261f4fd3ca64471c660271a2f39ede0
Dipjyoti Metia <dipjyoti.metia@beteasy.com.au>
2018-11-08 17:11:41 +1100

    added module 2

--- src/app.js
+++ src/app.js
@@ -10,8 +10,44 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf-8'
+);
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf-8'
+);
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => res.render('index', {
-    title: 'Index'
+    title: 'Account Summary',
+    accounts
 }));
+
+app.get('/savings', (req, res) => {
+    res.render('account', {
+        account: accounts.savings
+    });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', {
+        account: accounts.checking
+    });
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', {
+        account: accounts.credit
+    });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        user: users[0]
+    });
+});
+
 
 app.listen(3000, () => console.log('Ps project running on port 3000'))
